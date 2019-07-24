0c4ef43ef2002193c3621d169e16f64234b87304
pj smith <github@deathraylabs.com>
2019-07-24 10:52:24 -0500

    Changed code to utilize pathlib.Path functions. changed the order of code execution to catch None case before error is thrown.

--- scan_utilities.py
+++ scan_utilities.py
@@ -14,7 +14,7 @@ import re
 
 
 def files_with_matched_tags(working_dir, tags):
-    """Function that generates a list of files with matching extended
+    """Function generates a list of files with matching extended
     attribute tags in the finder.
 
     Args:
@@ -24,7 +24,7 @@ def files_with_matched_tags(working_dir, tags):
             tags you wish to match against
 
     Returns:
-        list: list of absolute paths to files that match the specified finder
+        list: list of Path objects for files that match the specified finder
             tags
         None: returned if no files match specified finder tag
 
@@ -32,10 +32,10 @@ def files_with_matched_tags(working_dir, tags):
 
     # The tag command can be found at https://github.com/jdberry/tag
     # tagCmd is location of tag executable on computer
-    tagCmd = "/usr/local/bin/tag"
+    tagCmd = Path("/usr/local/bin/tag")
 
     # strip the file name to ensure we're working with directory only
-    working_dir = os.path.dirname(working_dir)
+    working_dir = Path(working_dir).parent
 
     # change the current working directory to the location of audio files
     os.chdir(working_dir)
@@ -55,7 +55,7 @@ def files_with_matched_tags(working_dir, tags):
     tagged_files.sort()  # ensure files are in ascending order
 
     # takes the list of file names and creates a list of absolute paths
-    paths_to_tagged_files = [os.path.join(working_dir, path) for path in tagged_files]
+    paths_to_tagged_files = [working_dir / path for path in tagged_files]
 
     return paths_to_tagged_files
 
@@ -64,7 +64,8 @@ def merge_tagged_wav_files(wav_file_paths, output_path="merged.wav"):
     """ Simple function to combine multiple wav files into a single file.
 
     Args:
-        wav_file_paths (list): list of paths to wav files you wish to combine
+        wav_file_paths (list): list of Path objects for wav files you wish to
+            combine
         output_path (str): optional file name and path.
             - individual file names will be saved into CWD
             - specify full path name to save in a specific directory
@@ -75,21 +76,24 @@ def merge_tagged_wav_files(wav_file_paths, output_path="merged.wav"):
         (str): string containing path to newly created wav file
 
     """
+    if wav_file_paths is None:
+        print("No files contained specified tags.")
+        return False
+
     # container for wav files we wish to be merged
     combined_sounds = AudioSegment.empty()
 
+    # create our Path object
+    output_path = Path(output_path)
+
     # don't overwrite existing files
     # todo: this really needs to be a recursive function instead of this
-    if os.path.exists(output_path):
+    if output_path.exists():
         m = re.search(r"(.+_)(\d+)(\.wav)", output_path)
         try:
             output_path = m.group(1) + str(int(m.group(2)) + 1) + m.group(3)
         except AttributeError:
             output_path = output_path.replace(".wav", "_1.wav")
-
-    if wav_file_paths is None:
-        print("No files contained specified tags.")
-        return False
 
     for file in wav_file_paths:
         combined_sounds = combined_sounds + AudioSegment.from_wav(str(file))
