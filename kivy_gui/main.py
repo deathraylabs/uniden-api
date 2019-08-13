ac17fc39494c83e896b796b24a5dd1b4d2915072
pj smith <github@deathraylabs.com>
2019-08-12 21:16:25 -0500

    Created Kivy kv file to store formatting information.

--- kivy_gui/main.py
+++ kivy_gui/main.py
@@ -1,1 +1,19 @@
 """Learning to use Kivy GUI framework."""
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class DataWindow(Widget):
+    """Root element of Application UI."""
+
+    pass
+
+
+class DataWindowApp(App):
+    def build(self):
+        return DataWindow()
+
+
+if __name__ == "__main__":
+    DataWindowApp().run()
