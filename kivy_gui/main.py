e5ef46d5cf4985320925e0ef4550e6833f9c11e3
pj smith <github@deathraylabs.com>
2019-08-12 22:23:40 -0500

    Didn't get very far with Kivy tonight. No real logic, just pasted some stuff from tutorial.

--- kivy_gui/main.py
+++ kivy_gui/main.py
@@ -11,6 +11,17 @@ class DataWindow(Widget):
 
 
 class DataWindowApp(App):
+    """App class is called 'DataWindow', which means the 'kv' file should
+    have the same name. As long as it's in the same directory as the main.py
+    file it will be loaded at the same time.
+
+    All based on the `Kivy Tutorial`_
+
+    .. _`Kivy Tutorial`: https://github.com/kivy/kivy/wiki/Setting-Up-Kivy
+    -with-various-popular-IDE's#setting-up-kivy-with-pycharm-on-osx
+
+    """
+
     def build(self):
         return DataWindow()
 
