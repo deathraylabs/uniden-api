1decfaaf05dd282e59360e727c728533d844c391
pj smith <github@deathraylabs.com>
2019-08-31 21:12:29 -0500

    Changing import statement order.

--- kivy_gui/main.py
+++ kivy_gui/main.py
@@ -1,12 +1,20 @@
 """Learning to use Kivy GUI framework."""
+import sys
 import os
+
+if os.environ.get("TEXTDOMAIN") == "Linux-PAM":
+    print("On RPi")
+    sys.path.extend(["/home/pi/uniden-api", "/home/pi/uniden-api/scanner"])
+
+print("Checked if on RPi")
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.logger import Logger
 from kivy.core.audio import SoundLoader
 from kivy.clock import Clock
-from kivy.uix.widget import Widget
+
+# from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 # from kivy.uix.boxlayout import BoxLayout
@@ -259,12 +267,6 @@ class DataWindowApp(App):
 
 
 if __name__ == "__main__":
-    import sys
-    import os
-
-    if os.environ.get("TEXTDOMAIN") == "Linux-PAM":
-        print("On RPi")
-        sys.path.extend(["/home/pi/uniden-api", "/home/pi/uniden-api/scanner"])
 
     # path to wav_source that contains the audio of interest
     # wav_dir_path = (
